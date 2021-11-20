1d91aa05a8c28206b0ab91df004f3100d4b48a62
carlincb <carlin.colleen@gmail.com>
2021-11-20 15:52:18 -0700

    Changed to async/await routes.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,21 +4,30 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  Category.findAll({include: [Product]})
-  .then((data) => res.json(data))
-  .catch((err) => res.status(500).json(err))
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
-  Category.findOne({
-    where: {id: req.params.id},
-    include: [Product],
-  })
-  .then((data) => res.json(data))
-  .catch((err) => res.status(500).json(err))
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.post('/', (req, res) => {
